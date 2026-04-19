fa65d9dd3f84bb6da4a3d0dda3f59ca5b8d00462
phatdang <phatdang19052004@gmail.com>
2026-04-19 17:50:13 +0700

    feat(repository): add RoleRepository

---
+++ src/main/java/org/ping_me/repository/jpa/RoleRepository.java
@@ -0,0 +1,12 @@
+package org.ping_me.repository.jpa;
+
+import org.ping_me.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 16/04/2026, Thursday
+ **/
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
